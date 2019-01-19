1bbd267001656732fd09dfda65c3ea6f8808f826
Louis GE <gejian18@mails.ucas.edu.cn>
2019-01-19 06:42:58 +0000

    finish user signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get '/signup', to: 'users#new'
 
-  get 'static_pages/help'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/home'
+  get '/help', to: 'static_pages#help'
 
-  resources :microposts
-  resources :users
+  get '/home', to: 'static_pages#home'
+
   get 'welcome/index'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  
+  post '/signup', to: 'users#create'
 end
