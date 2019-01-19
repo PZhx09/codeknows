9b82e2879d7f6544b7751d00e0ec9d3ab5355058
Louis GE <gejian18@mails.ucas.edu.cn>
2019-01-19 13:44:53 +0000

    add microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -40,13 +41,6 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-    
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "请登录."
-        redirect_to login_url
-      end
     end
   
     def correct_user
