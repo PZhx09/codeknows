56d4f45f23fa29e012868b26a0b7ff403132fbf8
Louis GE <gejian18@mails.ucas.edu.cn>
2019-01-23 05:41:20 +0000

    improve extract function

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -3,35 +3,34 @@ module MicropostsHelper
     File.read('chinese_stop_words.txt').split.sort
   end
   
-  def rm_stopwords contents
-    stop_words.each do |stop_word|
-      if contents.include? stop_word 
-        contents = contents.delete(stop_word)
-      end
-    end
-    return contents
-  end
-  
   def rm_symbol text
     out = text.downcase
     out = out.gsub(/<[^<>]+>/, ' ')
     out = out.gsub(/[0-9]+/, ' ')
     out = out.gsub(/(http|https):\/\/[^\s]*/, ' ')
-    return out
   end
   
   def pre_content item
     title = rm_symbol item.title
     content = rm_symbol item.content
     contents = (title + ",") * 10 + "," + (content + ",") * 5
-    contents = rm_stopwords contents
+    return contents
   end
   
-  def extract_tags item
+  def extract_tags micro_post
     keyword = JiebaRb::Keyword.new
-    content = pre_content item
-    keywords_weights = keyword.extract content, 3
-    keywords = keywords_weights.map!{|word| word.first}.join(" | ")
-    returun keywords
+    contents = pre_content micro_post
+    keywords_weights = keyword.extract contents, 5
+    keywords = keywords_weights.map!{|word| word.first}
+    keywords.each do |word|
+      if stop_words.include?(word)
+        keywords = keywords.delete(word)
+      end
+    end
+    if keywords.length >= 3
+      return keywords.first(3).join(" | ")
+    else
+      return keywords.join(" | ")
+    end
   end
 end
